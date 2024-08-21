ef522e55a60ce954887ec28ed6dc111d977ad63a
unknown <SSDD@PNAWSA05LC1109.duoc.acad>
2024-08-21 10:32:05 -0400

    avance1

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarPage implements OnInit {
 
-  constructor() { }
+  Apellido: string = "";
+  Edad!: number;
+  Usuario: string = "";
+  constructor(private router:Router, private activaterouter:ActivatedRoute) {
+    this.activaterouter.queryParams.subscribe(param =>{
+      // valido si en la navegacion existe la variable de contexto
+      if (this.router.getCurrentNavigation()?.extras.state){
+        this.Usuario = this.router.getCurrentNavigation()?.extras?.state?.['user'];
+        this.Edad = this.router.getCurrentNavigation()?.extras?.state?.['eda'];
+        this.Apellido = this.router.getCurrentNavigation()?.extras?.state?.['ape'];
+
+      }
+    })
+   }
 
   ngOnInit() {
   }
